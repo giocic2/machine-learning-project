1e12313bf387abc317124f6cf5483c3ede259ad3
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-09-20 12:20:48 +0200

    minor changes

--- qlearning-1.py
+++ qlearning-1.py
@@ -10,17 +10,17 @@ print(env.action_space.n) # 0:"go left", 1:"stay", 2:"go right"
 
 # Q table, let's make the observation space discrete
 DISCRETE_OS_SIZE = [20,20] # observation
-discrete_os_win_size = (end.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
 print(discrete_os_win_size)
 # Q table initialization with random values
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n])) # 20x20x3 size (every possible action for every possible state (in the buckets).
 
-# done = False
-# 
-# while not done:
-#     action = 2
-#     newState, reward, done, _ = env.step(action) # underscore it's convention for throwaway variables.
-#     print("Reward: ", reward, "; New State: ", newState)
-#     env.render()
-#     
-# env.close()
+done = False
+
+while not done:
+    action = 2
+    newState, reward, done, _ = env.step(action) # underscore it's convention for throwaway variables.
+    print("Reward: ", reward, "; New State: ", newState)
+    env.render()
+    
+env.close()
